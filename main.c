52581f1df5ae2a09f3c0993ddea5a9368e0ebbaf
Danil Bibik <racta444@mail.ru>
2021-02-24 20:45:21 +0300

    Changed main file

--- main.c
+++ main.c
@@ -1,104 +1,56 @@
-#include <stdio.h>
-
-#define EMPTY -1
-
-#define WHITE 0
-#define BLACK 10
-
-#define PAWN 0
-#define ROOK 1
-#define KNIGHT 2
-#define BISHOP 3
-#define QUEEN 4
-#define KING 5
-
-int chessboard[8][8];
-int figures[] = {ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK};
-
-void printFigure(int figure, FILE* file)
-{
-    switch (figure) {
-    case EMPTY:
-        fprintf(file, " ");
-        break;
-    case WHITE + PAWN:
-        fprintf(file, "&#9817;");
-        break;
-    case WHITE + ROOK:
-        fprintf(file, "&#9814;");
-        break;
-    case WHITE + KNIGHT:
-        fprintf(file, "&#9816;");
-        break;
-    case WHITE + BISHOP:
-        fprintf(file, "&#9815;");
-        break;
-    case WHITE + QUEEN:
-        fprintf(file, "&#9812;");
-        break;
-    case WHITE + KING:
-        fprintf(file, "&#9813;");
-        break;
-    case BLACK + PAWN:
-        fprintf(file, "&#9823;");
-        break;
-    case BLACK + ROOK:
-        fprintf(file, "&#9820;");
-        break;
-    case BLACK + KNIGHT:
-        fprintf(file, "&#9822;");
-        break;
-    case BLACK + BISHOP:
-        fprintf(file, "&#9821;");
-        break;
-    case BLACK + QUEEN:
-        fprintf(file, "&#9818;");
-        break;
-    case BLACK + KING:
-        fprintf(file, "&#9819;");
-        break;
-    }
-}
-
-void fillChessboard(int chessboard[8][8])
-{
-    // Заполняем пустотой поле
-    for (int n = 0; n < 8; n++) {
-        for (int m = 0; m < 8; m++) {
-            chessboard[n][m] = EMPTY;
-        }
-    }
-    // Заполняем пешки
-    for (int i = 0; i < 8; i++) {
-        chessboard[1][i] = BLACK + PAWN;
-        chessboard[6][i] = WHITE + PAWN;
-    }
-    // Заполняем фигуры
-    for (int i = 0; i < 8; i++) {
-        chessboard[0][i] = BLACK + figures[i];
-        chessboard[7][i] = WHITE + figures[i];
-    }
-}
-
-void printChessboard(int chessboard[8][8])
-{
-    FILE* html_file;
-    html_file = fopen("result.html", "w+");
-    fprintf(html_file, "<html><body><table border='1'>");
-    for (int n = 0; n < 8; n++) {
-        fprintf(html_file, "<tr>");
-        for (int m = 0; m < 8; m++) {
-            fprintf(html_file, "<td height='21'>");
-            printFigure(chessboard[n][m], html_file);
-            fprintf(html_file, "</td>");
-        }
-        fprintf(html_file, "</tr>");
-    }
-    fprintf(html_file, "</table></body></html>");
-}
-
+#include <iostream>
+using namespace std;
 int main()
 {
-    fillChessboard(chessboard);
-    printChessboard(chessboard);
+char matr[8][8];
+char pawnw = 'P';
+char empty = ' ';
+char rookw = 'R';
+char knightw = 'N';
+char bishopw = 'B';
+char queenw = 'Q';
+char kingw = 'K';
+char pawnb = 'p';
+char rookb = 'r';
+char knightb = 'n';
+char bishopb = 'b';
+char queenb = 'q';
+char kingb = 'k';
+for(int i=0; i<8; i++)
+{
+  for(int j=0; j<8; j++)
+  {
+    if(i < 2) matr[i][j] = pawnw;
+    if(i > 5) matr[i][j] = pawnb;
+    if(i > 1 && i < 6) matr[i][j] = empty;
+  }
+}
+    matr[0][0] = rookw;
+    matr[0][1] = knightw;
+    matr[0][2] = bishopw;
+    matr[0][3] = queenw;
+    matr[0][4] = kingw;
+    matr[0][5] = bishopw;
+    matr[0][6] = knightw;
+    matr[0][7] = rookw;
+    matr[7][0] = rookb;
+    matr[7][1] = knightb;
+    matr[7][2] = bishopb;
+    matr[7][3] = queenb;
+    matr[7][4] = kingb;
+    matr[7][5] = bishopb;
+    matr[7][6] = knightb;
+    matr[7][7] = rookb;
+
+for(int i=0; i<8; i++)
+{
+  cout<< "|";
+  for(int j=0; j<8; j++)
+  {
+    cout << matr[i][j] << "|";
+  }
+  cout << "\n";
+}
+system("PAUSE");
+return 0;
 }
