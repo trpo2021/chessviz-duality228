85b888d94c3f18e2c06f5f022805673d0426a298
duality228 <78473394+duality228@users.noreply.github.com>
2021-04-20 20:25:27 +0300

    Update board.h

--- src/board.h
+++ src/board.h
@@ -6,11 +6,11 @@
 using namespace std;
 void Board(char* ptr[], int n, int m);
 void pBoard(char* ptr[], int n, int m);
-int Pawn(char* ptr[], int n, int m, char a[], int p, int hod);
-int pawn(char* ptr[], int n, int m, char a[], int p, int hod);
-int rook(char* ptr[], int n, int m, char a[], int p, int hod);
-int queen(char* ptr[], int n, int m, char a[], int p, int hod);
-int kNight(char* ptr[], int n, int m, char a[], int p, int hod);
-int bishop(char* ptr[], int n, int m, char a[], int p, int hod);
-int king(char* ptr[], int n, int m, char a[], int p, int hod);
+int Pawn(char* ptr[], int n, int m, char a[]);
+int pawn(char* ptr[], int n, int m, char a[]);
+int rook(char* ptr[], int n, int m, char a[], int hod);
+int queen(char* ptr[], int n, int m, char a[], , int hod);
+int kNight(char* ptr[], int n, int m, char a[], int hod);
+int bishop(char* ptr[], int n, int m, char a[], int hod);
+int king(char* ptr[], int n, int m, char a[],  int hod);
 #endif
