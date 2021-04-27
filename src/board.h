53442c45f3a66ecfa88c3536abac61a95235e1d3
duality228 <78473394+duality228@users.noreply.github.com>
2021-04-27 19:11:26 +0300

    Update board.h

--- src/board.h
+++ src/board.h
@@ -1,5 +1,5 @@
-#ifndef one
-#define one
+#ifndef BOARD_H
+#define BOARD_H
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,8 +8,8 @@ void Board(char* ptr[], int n, int m);
 void pBoard(char* ptr[], int n, int m);
 int Pawn(char* ptr[], int n, int m, char a[]);
 int pawn(char* ptr[], int n, int m, char a[]);
-int rook(char* ptr[], int n, int m, char a[]);
-int queen(char* ptr[], int n, int m, char a[]);
+int rook(char* ptr[], int n, int m, char* a, int hod);
+int queen(char* ptr[], int n, int m, char* a, int hod);
 int kNight(char* ptr[], int n, int m, char a[], int hod);
 int bishop(char* ptr[], int n, int m, char a[], int hod);
 int king(char* ptr[], int n, int m, char a[],  int hod);
